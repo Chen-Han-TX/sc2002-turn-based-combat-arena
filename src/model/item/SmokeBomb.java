d485a0fe02c3c808f94aa3410aa7ba8053c9df99
Chloe <Chloe@Chloe>
2026-03-25 04:10:45 +0800

    powerstone and smoke bomb

--- src/model/item/SmokeBomb.java
+++ src/model/item/SmokeBomb.java
@@ -1,5 +1,34 @@
 package model.item;
 
-public class SmokeBomb {
+import model.combatant.Combatant;
+import model.effect.SmokeBombEffect;
+import java.util.List;
 
+/**
+ * Owner: Person C
+ * Smoke Bomb: Enemy attacks deal 0 damage for current turn and next turn.
+ * Single use.
+ */
+public class SmokeBomb implements Item {
+    private boolean consumed = false;
+
+    @Override
+    public String getName() {
+        return "Smoke Bomb";
+    }
+
+    @Override
+    public void use(Combatant user, Combatant target, List<Combatant> allEnemies) {
+        if (consumed) {
+            return;
+        }
+
+        user.addStatusEffect(new SmokeBombEffect(2));
+        consumed = true;
+    }
+
+    @Override
+    public boolean isConsumed() {
+        return consumed;
+    }
 }
