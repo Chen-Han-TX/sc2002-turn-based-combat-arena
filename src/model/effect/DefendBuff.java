c60c9c9bba6da902415ddd11d294751bc59610e2
Chloe <Chloe@Chloe>
2026-03-25 04:08:57 +0800

    defendbuff

--- src/model/effect/DefendBuff.java
+++ src/model/effect/DefendBuff.java
@@ -1,5 +1,43 @@
 package model.effect;
 
-public class DefendBuff {
+import model.combatant.Combatant;
 
+public class DefendBuff implements StatusEffect {
+    private int turnsRemaining;
+
+    public DefendBuff(int turnsRemaining) {
+        this.turnsRemaining = turnsRemaining;
+    }
+
+    @Override
+    public String getName() {
+        return "Defend Buff";
+    }
+
+    @Override
+    public void applyEffect(Combatant target) {
+        // Leave empty if defense bonus is handled elsewhere
+    }
+
+    @Override
+    public void tick() {
+        if (turnsRemaining > 0) {
+            turnsRemaining--;
+        }
+    }
+
+    @Override
+    public boolean preventsAction() {
+        return false;
+    }
+
+    @Override
+    public boolean isExpired() {
+        return turnsRemaining <= 0;
+    }
+
+    @Override
+    public void onExpire(Combatant target) {
+        // Leave empty
+    }
 }
