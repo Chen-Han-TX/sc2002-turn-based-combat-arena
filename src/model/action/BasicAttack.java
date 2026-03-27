c1730e8201a583a3c42bfb2f44f61afe3bd06d52
hengping <hengping.tenby@gmail.com>
2026-03-27 14:43:52 +0800

    Feat: Added Basic Attack

--- src/model/action/BasicAttack.java
+++ src/model/action/BasicAttack.java
@@ -16,11 +16,11 @@ public class BasicAttack implements Action {
 
     @Override
     public void execute(Combatant performer, Combatant target, List<Combatant> allEnemies) {
-        // TODO: implement damage calculation
-        // damage = max(0, performer.getAttack() - target.getDefense())
-        // target.takeRawDamage(damage)  <-- use raw since we calc ourselves
+               int damage = Math.max(0, performer.getAttack() - target.getDefense());
+        target.takeRawDamage(damage);
+        System.out.println(performer.getName() + " deals " + damage + " damage to " + target.getName());
     }
-
+    
     @Override
     public boolean isAvailable(Combatant performer) {
         return true; // always available
